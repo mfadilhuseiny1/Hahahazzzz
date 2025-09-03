4c1420c385b6833aa1da5b8185e2a22d71e70be3
mfadilhuseiny1 <mfadilhuseiny1@gmail.com>
2025-09-03 14:02:05 +0700

    .env pm2

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -5,6 +5,7 @@ module.exports = {
     args: 'start',
     cron_restart: '0 */1 * * *', // Auto restart setiap 1 jam
     max_memory_restart: '800M',
+    env_file: '.env',
     env: {
       NODE_ENV: 'production'
     }
